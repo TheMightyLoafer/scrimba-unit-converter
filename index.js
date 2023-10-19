9f21fa33490ca4e6fa556fc7fd05508789c2e5e0
KingOfBoopp <107333538+KingOfBoopp@users.noreply.github.com>
2023-10-19 07:47:40 -0700

    v0.2
    Started working on conversion formula and hooking it up

--- index.js
+++ index.js
@@ -3,3 +3,17 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const convertBtn = document.getElementById("convert")
+const inputField = document.getElementById("input")
+let conversion;
+
+addEventListener("click", function() {
+    convertToMeters(inputField.value)
+})
+
+function convertToMeters (input) {
+    input = input * 0.3048
+    return Math.floor(input * 1000) / 1000
+}
+
