8acad6cbd7a71defe9e8bfff7f6227e15de3f86d
Nick Jones <147700520+TheMightyLoafer@users.noreply.github.com>
2023-10-20 01:43:10 -0700

    logic fixes
    -fixed functions
    -added a slider button
    -need to add dark/lightmode functionality

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
 /*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+Length (Feet/Meters)
+1 foot = 0.3048 meters
+1 meter = 3.28084 feet
 
-const meterToFeet = 3.281
-const literToGallon = 0.264
-const kilogramToPound = 2.204
+Weight (Pounds/Kilograms)
+1 pound = 0.45359237 kilograms
+1 kilogram = 2.20462262 pounds
+
+Volume (Gallons/Liters)
+1 gallon = 3.78541178 liters
+1 liter = 0.26417205 gallons
+*/
 
 const convertBtn = document.getElementById("convert")
 const inputField = document.getElementById("input")
 const metersEl = document.getElementById("p-meters")
-
-//let inputValue = inputField.value
-//.textContent = "0 Feet = 0.000 Meters | 0 Meters = 0.000 Feet"
-let conversion;
-
+const poundsEl = document.getElementById("p-pounds")
+const gallonsEl = document.getElementById("p-gallons")
 
 addEventListener("click", function() {
-    let inputValue = inputField.value
-    conversion = convertFunction(inputValue)
-    metersEl.textContent = `${inputValue} Feet = ${conversion} Meters`
+    let value = inputField.value
+    const conversions = convertUnits(value)
+    metersEl.textContent = `${value} Feet = ${conversions.feet} Meters | ${value} Meters = ${conversions.meters} Feet`
+    poundsEl.textContent = `${value} Pounds = ${conversions.pounds} Kilograms | ${value} Kilograms = ${conversions.kilograms} Pounds`
+    gallonsEl.textContent = `${value} Gallons = ${conversions.gallons} Liters | ${value} Liters = ${conversions.liters} Gallons`
 })
 
-function convertFunction(inputValue) {
+function convertUnits(value) {
     const conversionFactors = {
         feet: 0.3048,
         meters: 3.28084,
-        miles: 0.000189394
+        pounds: 0.45359237,
+        gallons: 3.78541178,
+        liters: 0.26417205,
+        kilograms: 2.20462262
     }
 
     const results = {}
 
     for (const [unit, factor] of Object.entries(conversionFactors))
     {
-        results[unit] = value * factor
+        results[unit] = Math.floor(value * factor * 1000) / 1000
     }
 
     return results
@@ -42,7 +48,6 @@ function convertFunction(inputValue) {
 
 /* 
 
-```javascript
 function convertUnits(value) {
     const conversionFactors = {
         feet: 0.3048,
@@ -58,14 +63,10 @@ function convertUnits(value) {
 
     return results
 }
-```
 
-This function takes a `value` parameter and returns an object containing the converted values for feet, meters, and miles. You can call this function with a value and then access the converted values like this:
 
-```javascript
 const conversions = convertUnits(10)
 console.log(conversions.feet) // Output: 32.8084
 console.log(conversions.meters) // Output: 3.048
 console.log(conversions.miles) // Output: 0.00189394
-```
 */
