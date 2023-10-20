8d0fed8d223fcb1813d389863f01e9931e1911f3
KingOfBoopp <147700520+TheMightyLoafer@users.noreply.github.com>
2023-10-19 17:08:01 -0700

    worked on the math converstions

--- index.js
+++ index.js
@@ -4,17 +4,68 @@
 1 kilogram = 2.204 pound
 */
 
+const meterToFeet = 3.281
+const literToGallon = 0.264
+const kilogramToPound = 2.204
+
 const convertBtn = document.getElementById("convert")
 const inputField = document.getElementById("input")
 const metersEl = document.getElementById("p-meters")
+
+//let inputValue = inputField.value
+//.textContent = "0 Feet = 0.000 Meters | 0 Meters = 0.000 Feet"
 let conversion;
 
+
 addEventListener("click", function() {
-    conversion = convertToMeters(inputField.value)
-    metersEl.textContent = `${inputField.value} Feet = ${conversion} Meters`
+    let inputValue = inputField.value
+    conversion = convertFunction(inputValue)
+    metersEl.textContent = `${inputValue} Feet = ${conversion} Meters`
 })
 
-function convertToMeters (input) {
-    input = input * 0.3048
-    return Math.floor(input * 1000) / 1000
+function convertFunction(inputValue) {
+    const conversionFactors = {
+        feet: 0.3048,
+        meters: 3.28084,
+        miles: 0.000189394
+    }
+
+    const results = {}
+
+    for (const [unit, factor] of Object.entries(conversionFactors))
+    {
+        results[unit] = value * factor
+    }
+
+    return results
 }
+
+/* 
+
+```javascript
+function convertUnits(value) {
+    const conversionFactors = {
+        feet: 0.3048,
+        meters: 3.28084,
+        miles: 0.000189394
+    }
+
+    const results = {}
+
+    for (const [unit, factor] of Object.entries(conversionFactors)) {
+        results[unit] = value * factor
+    }
+
+    return results
+}
+```
+
+This function takes a `value` parameter and returns an object containing the converted values for feet, meters, and miles. You can call this function with a value and then access the converted values like this:
+
+```javascript
+const conversions = convertUnits(10)
+console.log(conversions.feet) // Output: 32.8084
+console.log(conversions.meters) // Output: 3.048
+console.log(conversions.miles) // Output: 0.00189394
+```
+*/
